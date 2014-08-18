6408fcae0a60df2af9cbc803b2f4d84e01150ef5
unknown <ntyet@ts4-stat.iastate.edu>
2014-08-17 19:07:48 -0500

    edit error

--- MS_K_20_pbeta_0.R
+++ MS_K_20_pbeta_0.R
@@ -571,7 +571,7 @@ sim_QLfit <- function(p.beta, i.beta, e.beta, S, L, U){
   return(res)
 }
 #### simulation #####
-#n.sim <- 1; j <- 1; i <- 1
+#n.sim <- 2; j <- 1; i <- 1
 out_20_0 <- llply(1:length(i.beta), function(j){
   out1 <- laply(1:n.sim, function(i){
     sim1 <- sim_QLfit(p.beta, i.beta[j], e.beta[j], S, L, U)
